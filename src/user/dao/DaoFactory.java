942636ae62eff2791fc082cb9fdc58118e2bc4b9
chicichich <pch6789@naver.com>
2021-04-19 14:46:42 +0900

    1.5.1 오브젝트 팩토리를 이용한 스프링 IoC

--- src/user/dao/DaoFactory.java
+++ src/user/dao/DaoFactory.java
@@ -1,9 +1,15 @@
 package user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(mysqlConnectionMaker());
     }
+    @Bean
     public ConnectionMaker mysqlConnectionMaker(){
         return new MySqlConnectionMaker();
     }

2b262f622af9674702308b849a4fd08ce979f49f
chicichich <pch6789@naver.com>
2021-04-29 10:44:53 +0900

    2.4.2 JUnit 테스트 오브젝트 테스트

---
+++ src/learningtest/junit/JUnitTest.java
@@ -0,0 +1,31 @@
+package learningtest.junit;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class JUnitTest {
+    static Set<JUnitTest> testObjects = new HashSet<>();
+
+    @Test
+    public void test1() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test2() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test3() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+}
